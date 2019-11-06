72527784686db895ba7f833841caa82f92e2f604
Jorji Costava <gasymov.df18@physics.msu.ru>
2019-11-07 00:14:14 +0300

    Add change aperture radius

--- scripts/check.py
+++ scripts/check.py
@@ -6,6 +6,7 @@ from PIL import Image
 from astropy import wcs
 import astropy.units as u
 from astropy.io import fits
+from astropy.io import ascii
 from astropy.time import Time
 import matplotlib.pyplot as plt
 from astropy.table import Column
@@ -36,6 +37,15 @@ def format2_wcs(file):
             break
     return file.replace(file[k:], ".wcs")
 
+def get_data(file):
+    if '.jpg' in file:
+        red, green, blue = jpg2_fits(file) 
+        data = green
+    else:
+        fits_file = file
+        hdulist = fits.open(fits_file)
+        data = hdulist[0].data[1]
+    return data
 
 def CR_altaz(hdu, location, time):
     header = hdu[0].header
@@ -74,12 +84,22 @@ def create_wcs(grid, location, time):
         with fits.open(file, mode='update') as hdu:
             header = hdu[0].header
             az, alt = header['CRVAL1_AZ'], header['CRVAL2_ALT']
-            print(header['CRVAL1'], header['CRVAL2'])
             CR = SkyCoord(az*u.deg, alt*u.deg, obstime=time, location=location, frame = 'altaz')       
             header['CRVAL1'] = CR.transform_to('icrs').ra.degree
             header['CRVAL2'] = CR.transform_to('icrs').dec.degree
-            print(header['CRVAL1'], header['CRVAL2'])
             hdu.flush() 
+
+def Calculate_aperture(BSCatalogue, min_rad=5, increase_per_100px=5):
+    p = np.array(BSCatalogue['Distance_center'])
+    radii = min_rad + increase_per_100px * p/100
+    return radii
+
+def subtract_star(data, x, y, STAR_x, STAR_y, radius, STAR_SIZE):
+    a, b = len(data[0]), len(data)
+    for i in range(max(int(x - radius + STAR_x - STAR_SIZE), 0), min(int(x - radius + STAR_x + STAR_SIZE + 1), a)):
+        for j in range(max(int(y - radius + STAR_y - STAR_SIZE), 0), min(int(y - radius + STAR_y + STAR_SIZE + 1), b)):
+            data[j][i] = 0
+    return data
 
 
 def BSC_open(file):
@@ -101,17 +121,23 @@ def BSC_open(file):
     BSCatalogue['ra'] = BSCatalogue['ra'].astype(float)
     BSCatalogue['dec'] = BSCatalogue['dec'].astype(float)
     BSCatalogue['vmag'] = BSCatalogue['vmag'].astype(float)
+    BSCatalogue.sort('vmag')
     return BSCatalogue
 
 
-def BSC_xy(BSCatalogue, wcs):
+def BSC_xy(BSCatalogue, wcs, data):
     BSCatalogue.add_column(Column(np.zeros(len(BSCatalogue))), name='x')
     BSCatalogue.add_column(Column(np.zeros(len(BSCatalogue))), name='y')
+    BSCatalogue.add_column(Column(np.zeros(len(BSCatalogue))), name='Distance_center')
+    a, b = len(data[0]), len(data)
+
     for i in range(len(BSCatalogue)):
         BSCatalogue[i]['x'], BSCatalogue[i]['y'] = wcs.wcs_world2pix(
             BSCatalogue[i]['ra'], BSCatalogue[i]['dec'], 0)
         BSCatalogue[i]['x'] = round(BSCatalogue[i]['x'])
         BSCatalogue[i]['y'] = round(BSCatalogue[i]['y'])
+        x, y = BSCatalogue[i]['x'], BSCatalogue[i]['y']
+        BSCatalogue[i]['Distance_center'] = np.sqrt((x - a/2)**2 + (y - b/2)**2)
     return BSCatalogue
 
 
@@ -144,9 +170,10 @@ def BSC_extinction(BSCatalogue):
     return BSCatalogue
 
 
-def BSC_observ_flux(BSCatalogue, data, STAR=10, CRIT_m=5.):
+def BSC_observ_flux(BSCatalogue, data, STAR=None, CRIT_m=5.):
     a, b = len(data[0]), len(data)
     FLUX_0m = 0
+    STAR_SIZE = 5
     num = 0
     BSCatalogue.add_column(Column(np.zeros(len(BSCatalogue))), name='Observ_flux')
 
@@ -154,13 +181,23 @@ def BSC_observ_flux(BSCatalogue, data, STAR=10, CRIT_m=5.):
         x, y = BSCatalogue[i]['x'], BSCatalogue[i]['y']
         m = BSCatalogue[i]['vmag']
         if (x >= 0 and x < a) and (y >= 0 and y < b) and (m < CRIT_m):
-            STAR_data = Cutout2D(data, (x, y), (2*STAR + 1, 2*STAR + 1)).data
-            mean_star, median_star, std_star = sigma_clipped_stats(
-                STAR_data, sigma=5.0)
-            STAR_data = STAR_data - median_star
+            if STAR is None:
+                radius = 15
+            else:
+                radius = STAR[i]
+            STARregion_data = Cutout2D(data, (x, y), (2*radius + 1, 2*radius + 1)).data
+
+            mean_starregion, median_starregion, std_starregion = sigma_clipped_stats(
+                STARregion_data, sigma=5.0)
+            STARregion_data = STARregion_data - median_starregion
+            STAR_y, STAR_x = np.unravel_index(np.argmax(STARregion_data), STARregion_data.shape)
+            STAR_data = Cutout2D(STARregion_data, (STAR_x, STAR_y), (2*STAR_SIZE + 1, 2*STAR_SIZE + 1)).data
 
             BSCatalogue[i]['Observ_flux'] = photometry(STAR_data)
-            zero_point = BSCatalogue[i]['Observ_flux']/(2.512**m)
+            data = subtract_star(data, x, y, STAR_x, STAR_y, radius, STAR_SIZE)
+            STARregion_data = subtract_star(STARregion_data, 0, 0, STAR_x, STAR_y, 0, STAR_SIZE)
+
+            zero_point = BSCatalogue[i]['Observ_flux'] * (2.512**m)
             FLUX_0m += zero_point
             num += 1
     return BSCatalogue, FLUX_0m/num
@@ -169,12 +206,13 @@ def BSC_observ_flux(BSCatalogue, data, STAR=10, CRIT_m=5.):
 def BSC_theory_flux(BSCatalogue, FLUX_0m=1000):
     BSCatalogue.add_column(Column(np.zeros(len(BSCatalogue))), name='Flux')
     for i in range(len(BSCatalogue)):
-        BSCatalogue[i]['Flux'] = 2.512**(-float(BSCatalogue[i]['vmag'])) * FLUX_0m
+        BSCatalogue[i]['Flux'] = 2.512**(-float(BSCatalogue[i]['vmag_atmosph'])) * FLUX_0m
     return BSCatalogue
 
 
-def make_map(BSCatalogue, data, outfile, CIRCLE_RADIUS=20., lw=1., OBS_TH_value=1.5):
+def make_map(BSCatalogue, data, outfile, circle_radii=None, lw=1., OBS_TH_value=1.5):
     a, b = len(data[0]), len(data)
+    g, r = 0, 0
     plt.figure(figsize=(8, 13))
     plt.imshow(data, cmap='Greys', origin='lower', norm=LogNorm())
     for i in range(len(BSCatalogue)):
@@ -182,24 +220,32 @@ def make_map(BSCatalogue, data, outfile, CIRCLE_RADIUS=20., lw=1., OBS_TH_value=
             x, y = BSCatalogue[i]['x'], BSCatalogue[i]['y']
             if BSCatalogue[i]['Observ_flux'] < BSCatalogue[i]['Flux'] / OBS_TH_value:
                 color = 'red'
+                r+=1
             else:
                 color = 'green'
+                g+=1
+            if 'Alp' in BSCatalogue[i]['alt_name  ']:
+                plt.text(x + 30, y + 30, BSCatalogue[i]['alt_name  '])
+            if circle_radii is None:
+                CIRCLE_RADIUS = 30
+            else:
+                CIRCLE_RADIUS = circle_radii[i]
             aperture = CircularAperture((x, y), r=CIRCLE_RADIUS)
             aperture.plot(color=color, lw=lw)
+    plt.plot([], [], ' ', label='min star flux (Obs/Th) = ' +
+              str(np.round(OBS_TH_value, 3)))
+    plt.plot([], [], ' ', label='green/(red + green) = ' +
+              str(np.round(g/(r + g), 3)))
+    plt.legend(loc='best')
+    plt.xlim(0, a)
+    plt.ylim(0, b)
     plt.tight_layout()
     plt.savefig(outfile)
     plt.close()
 
 
 def processed(file):
-    if '.jpg' in file:
-        red, green, blue = jpg2_fits(file) 
-        data = green
-    else:
-        fits_file = file
-        hdulist = fits.open(fits_file)
-        data = hdulist[0].data[1]
-    STAR_size = 15
+    data = get_data(file)
     KGO = EarthLocation(lat='43d44m46s', lon='42d40m03s', height=2112*u.m)
     time = Time('2019-10-15T00:00:00.1', format='isot', scale='utc')
 
@@ -214,13 +260,16 @@ def processed(file):
 
     BSC_file = "../data/BSC_clean.txt"
     BSCatalogue = BSC_open(BSC_file)
-    BSCatalogue = BSC_xy(BSCatalogue, w)
-    BSCatalogue, FLUX_0m = BSC_observ_flux(BSCatalogue, data, STAR=STAR_size, CRIT_m=5.)
+    BSCatalogue = BSC_xy(BSCatalogue, w, data)
+    Aperture_radii = Calculate_aperture(BSCatalogue, min_rad=10, increase_per_100px=5)
+    BSCatalogue, FLUX_0m = BSC_observ_flux(BSCatalogue, data, STAR=Aperture_radii, CRIT_m=5.)
 
     BSCatalogue = BSC_altaz(BSCatalogue, KGO, time)
     BSCatalogue = BSC_extinction(BSCatalogue)
     BSCatalogue = BSC_theory_flux(BSCatalogue, FLUX_0m=FLUX_0m)
-    make_map(BSCatalogue, data, '../processed/result.jpg', CIRCLE_RADIUS=STAR_size, lw=2., OBS_TH_value=1.2)
+    ascii.write(BSCatalogue, 'BSCatalogue.txt')  
+    data = get_data(file)
+    make_map(BSCatalogue, data, '../processed/result.jpg', circle_radii=Aperture_radii, lw=2., OBS_TH_value=1.2)
 
 
 file = '../data/Cas.jpg'
