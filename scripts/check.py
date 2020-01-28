9c5eea35ecc1ffdfa813918d6c23e5a43a8f8dc4
Jorji Costava <gasymov.df18@physics.msu.ru>
2020-01-28 22:13:38 +0300

    Add some features

--- scripts/check.py
+++ scripts/check.py
@@ -26,23 +26,24 @@ from astropy.coordinates import SkyCoord, AltAz
 from astropy.nddata.utils import Cutout2D
 from astropy.stats import sigma_clipped_stats
 from astropy.coordinates import EarthLocation
+from astropy.coordinates import get_moon, get_sun
 from astropy.utils import iers
 import warnings
 from astropy.utils.exceptions import AstropyWarning
 iers.conf.auto_download = False
 warnings.simplefilter('ignore', AstropyWarning)
 
-
 def get_data(file, time=None):
     '''
     Получение данных с изображения (jpg или FITS). 
     Для FITS файла проводится нормировка потока, если есть FNUMBER и изменяется момент съёмки если есть TIME.
     '''
+    camera_ip = 1
     if '.jpg' in file:
         red, green, blue = jpg2_fits(file)
         data = green
         if time is not None:
-            return data, time
+            return data, time, camera_ip
     else:
         fits_file = file
         hdulist = fits.open(fits_file)
@@ -53,9 +54,7 @@ def get_data(file, time=None):
         if time is not None:
             if header['TIME']:
                 time = header['TIME']
-                return data, time
-    return data
-
+        return data, time, camera_ip
 
 def bkg(data, SNR=5, box_size=30, filter_size=3):
     '''
@@ -66,8 +65,9 @@ def bkg(data, SNR=5, box_size=30, filter_size=3):
     bkg = Background2D(data, (box_size, box_size), filter_size=(filter_size, filter_size),
                        sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
     #hdu = fits.PrimaryHDU()
+    mean_bkg = np.mean(bkg.background)
     fits.writeto('background.fits', bkg.background, overwrite=True)
-    return data - bkg.background
+    return data - bkg.background, mean_bkg
 
 
 def jpg2_fits(file):
@@ -79,6 +79,19 @@ def jpg2_fits(file):
     data_g = np.array(rgb[1].getdata()).reshape(ysize, xsize)
     data_b = np.array(rgb[2].getdata()).reshape(ysize, xsize)
     return data_r, data_g, data_b
+
+
+def WhereMoon(loc, time):
+    moon = get_moon(time, loc)
+    sun = get_sun(time)
+    elongation = sun.separation(moon)
+    ph_angle = np.arctan2(sun.distance*np.sin(elongation),
+                      moon.distance - sun.distance*np.cos(elongation))
+    ph = (1 + np.cos(ph_angle))/2.
+
+    altaz_moon = moon.transform_to(AltAz(obstime=time, location=loc))
+    altaz_sun = sun.transform_to(AltAz(obstime=time, location=loc))
+    return ph, altaz_moon.az.deg, altaz_moon.alt.deg, altaz_sun.az.deg, altaz_sun.alt.deg
 
 
 def create_wcs(file, location, time):
@@ -101,33 +114,12 @@ def photometry(STAR_data):
     '''
     return np.sum(STAR_data)
 
-
-def BSC_open(file):
-    '''
-    Открытие каталога Bright Stars. Поправка некоторых ошибок при чтении каталога,
-    пересчёт (RA, DEC) в десятичный формат.
-    '''
-    BSCatalogue = pd.read_csv(file, sep='|')
-    BSCatalogue = BSCatalogue.drop(BSCatalogue.columns[0], axis=1)
-    BSCatalogue = BSCatalogue.drop(BSCatalogue.columns[-1], axis=1)
-    BSCatalogue = QTable.from_pandas(BSCatalogue)
-    for i in BSCatalogue.colnames:
-        BSCatalogue[i].name = i.replace(' ', '')
-
-    for i in BSCatalogue:
-        ra = [float(j) for j in i['ra'].split()]
-        dec = [float(j) for j in i['dec'].split()]
-
-        i['ra'] = str(np.round((ra[0] + ra[1]/60 + ra[2]/(60*60))*15, 8))
-        if dec[0] > 0:
-            i['dec'] = str(np.round(dec[0] + dec[1]/60 + dec[2]/(60*60), 8))
-        else:
-            i['dec'] = str(np.round(dec[0] - dec[1]/60 - dec[2]/(60*60), 8))
-    BSCatalogue['ra'] = BSCatalogue['ra'].astype(float)
-    BSCatalogue['dec'] = BSCatalogue['dec'].astype(float)
-    BSCatalogue['vmag'] = BSCatalogue['vmag'].astype(float)
-    BSCatalogue.sort('vmag')
-    return BSCatalogue
+def bkg_mag(mean_bkg, wcs_file):
+    fits_wcs = fits.open(wcs_file)
+    header = fits_wcs[0].header
+    pxl_wcs = wcs.WCS(header)
+    scale = wcs.utils.proj_plane_pixel_scales(pxl_wcs)
+    return -2.5*np.log10(mean_bkg/((scale[0]*3600)**2)/header['zeropoint'])
 
 
 def BSC_xy(BSCatalogue, wcs_file, data, edge=400, min_res=5):
@@ -246,53 +238,137 @@ def BSC_theory_flux(BSCatalogue, wcs_file):
                                   ) * header['sigma_zeropoint']
     return BSCatalogue
 
+def make_constell(ax, data, wcs_file, HIP_file, cnstl_file):
+    a, b = len(data[0]), len(data)
+    w = wcs.WCS(wcs_file)
+    hdu = fits.open(wcs_file)
+    header = hdu[0].header
 
-def make_map(BSCatalogue, file, data, outfile, lw=1.):
+    HIPcat = ascii.read(HIP_file, format='csv', fast_reader=True) 
+    f = open(cnstl_file)
+    cons_dict = {}
+    for line in f:
+        words = line.split()
+        connects = [int(i) for i in words[2:]]
+        cons_dict[words[0]] = connects
+    for constell in cons_dict:
+        cnct = cons_dict[constell]        
+        for i in range(0, len(cnct), 2):
+            ra_s1 = float(HIPcat[HIPcat['num'] == cnct[i]]['ra'])
+            dec_s1 = float(HIPcat[HIPcat['num'] == cnct[i]]['dec'])
+            ra_s2 = float(HIPcat[HIPcat['num'] == cnct[i+1]]['ra'])
+            dec_s2 = float(HIPcat[HIPcat['num'] == cnct[i+1]]['dec'])
+            xt1, yt1 = w.wcs_world2pix(ra_s1, dec_s1, 0)
+            xt2, yt2 = w.wcs_world2pix(ra_s2, dec_s2, 0)
+            if (np.isnan(xt1) or np.isnan(yt1) or (xt1 <= -edge or xt1 >= a+edge) or (yt1 <= -edge or yt1 >= b+edge)) and \
+                (np.isnan(xt2) or np.isnan(yt2) or (xt2 <= -edge or xt2 >= a+edge) or (yt2 <= -edge or yt2 >= b+edge)):
+                continue
+            x1, y1 = clt.real_xy(xt1, yt1, header)
+            x2, y2 = clt.real_xy(xt2, yt2, header)
+            ax.plot([x1, x2], [y1, y2], lw=1, color='yellow')    
+
+
+def make_eqgrid(ax, data, wcs_file):
+    a, b = len(data[0]), len(data)
+    w = wcs.WCS(wcs_file)
+    hdu = fits.open(wcs_file)
+    header = hdu[0].header
+
+    for dec in range(-90, 90, 20):
+        dec_x = []
+        dec_y = []
+        for ra_i in range(0, 360, 1):
+            xt, yt = w.wcs_world2pix(ra_i, dec, 0)
+            if np.isnan(xt) or np.isnan(yt) or (xt <= -edge or xt >= a+edge) or (yt <= -edge or yt >= b+edge):
+                continue
+            x, y = clt.real_xy(xt, yt, header)
+            dec_x.append(x)
+            dec_y.append(y)
+        ax.plot(dec_x, dec_y, color='blue', lw = 0.5)
+
+    for ra in range(0, 360, 30):
+        ra_x = []
+        ra_y = []
+        for dec_i in range(-90, 90, 1):
+            xt, yt = w.wcs_world2pix(ra, dec_i, 0)
+            if np.isnan(xt) or np.isnan(yt) or (xt <= -edge or xt >= a+edge) or (yt <= -edge or yt >= b+edge):
+                continue
+            x, y = clt.real_xy(xt, yt, header)
+            ra_x.append(x)
+            ra_y.append(y)
+        ax.plot(ra_x, ra_y, color='blue', lw = 0.5)
+
+def make_map(BSCatalogue, file, data, outfile, wcs_file, HIP_file, cnstl_file, lw=1.):
     '''
     Создание карты облачности с отметкой проходящих и непроходящих анализ звёзд.
     Анализ: если видимый поток меньше на 2*std от теоретического потока, то мы отмечаем "облако".
     Зелёный кружок -- звезда проходит тест, красный -- на месте звезды "облако".
     Для получения карты прозрачности на данный момент не хватает точности измерений нуль-пункта.
     '''
+    BSCatalogue.add_column(Column(np.zeros(len(BSCatalogue))), name='Cloudiness')
     a, b = len(data[0]), len(data)
     g, r = 0, 0
     fig = plt.figure()  # figsize=(13, 8)
     ax = fig.add_axes([0, 0, 1, 1])
     ax.imshow(data, cmap='Greys', origin='lower', norm=LogNorm())
-    # make_constellations()
+    make_eqgrid(ax, data, wcs_file)
+    make_constell(ax, data, wcs_file, HIP_file, cnstl_file)
 
     for i in BSCatalogue:
         x, y = i['x'], i['y']
-        if i['Observ_flux'] < i['Flux'] - i['sigma_Flux']*2:
-            color = 'red'
-            r += 1
-        else:
-            color = 'green'
-            g += 1
+        obs, th = i['Observ_flux'], i['Flux']
+        i['Cloudiness'] = 1 - np.max((np.min((obs/th, 1)), 0))
 
+    for i in BSCatalogue:
+        x, y = i['x'], i['y']
+        cl = i['Cloudiness']
+        '''r = []
+        for j in BSCatalogue:
+            if i['name']!=j['name']:
+                r.append(((x - j['x'])**2 + (y - j['y'])**2)**0.5)
+        rad = np.min(r)'''
         if 'Alp' in i['alt_name']:
-            ax.text(x + 30, y + 30, i['alt_name'])
-        circle = plt.Circle((x, y), 20, color=color, lw=lw, fill=False)
-        ax.add_artist(circle)
+           ax.text(x + 30, y + 30, i['alt_name'])
+        if cl > 0.4:
+            circle = plt.Circle((x, y), 50, color=((255*cl)/255, (255*(1-cl))/255, 0), lw=lw, fill=True, alpha = 0.5)
+            ax.add_artist(circle)
 
-    ax.plot([], [], ' ', label='green/(red + green) = ' +
-            str(np.round(g/(r + g), 3)))
-    ax.legend(loc='best')
+    cloud = np.mean(BSCatalogue['Cloudiness'])
     ax.set_xlim(0, a)
     ax.set_ylim(b, 0)
     ax.set_title('Cloud map for ' + file)
-    ax.legend()
     # fig.tight_layout()
     fig.savefig(outfile)
     plt.close()
+    '''
+    cloud = plt.figure()  # figsize=(13, 8)
+    ax_cloud = cloud.add_axes([0, 0, 1, 1])
+    map = np.zeros((b, a))
+    for i in range(0, b, 20):
+        for j in range(0, a, 20):
+            r = []
+            for star in BSCatalogue:
+                x, y = star['x'], star['y']
+                r.append(((x - i)**2 + (y - j)**2)**0.5)
+            nrst = np.argmin(r)
+            obs, th = BSCatalogue['Observ_flux'][nrst], BSCatalogue['Flux'][nrst]
+            map[i][j] = np.max((np.min((obs/th, 1)), 0))
+    ax_cloud.imshow(map, cmap='magenta')
+    ax.set_title('Cloud map for ' + file)
+    # fig.tight_layout()
+    fig.savefig('diff_map_' + outfile)
+    plt.close()'''
+    return cloud
 
 
 def processed(file, outfile, calibrate=True):  # Основная процедура программы
-    data, t = get_data(file, TIME)
-    data = bkg(data, SNR=SNR, box_size=box_size,
+    print("Working on: {}".format(file))
+    data, t, camera_ip = get_data(file, TIME)
+    data, mean_bkg = bkg(data, SNR=SNR, box_size=box_size,
                filter_size=filter_size)
     loc = EarthLocation(lat=lat, lon=lon, height=height*u.m)
     time = Time(t, format='isot', scale='utc')
+    ph, moon_alt, moon_az, sun_alt, sun_az = WhereMoon(loc, time)
 
     grid = glob.glob('../data/*.wcs')
     if len(grid) == 0:  # Сетки нет
@@ -305,8 +381,10 @@ def processed(file, outfile, calibrate=True):  # Основная процеду
         else:
             create_wcs(grid[0], loc, time)
     wcs_file = glob.glob('../data/*.wcs')[0]
+    bkg_magnitd = bkg_mag(mean_bkg, wcs_file)
 
-    BSCatalogue = BSC_open(BSC_file)
+    BSCatalogue = ascii.read(BSC_file, format='csv', fast_reader=True) 
+    # BSCatalogue = BSC_open(BSC_file) # Работает на 5 секунд быстрее?
     BSCatalogue = BSC_xy(BSCatalogue, wcs_file, data, min_res=res_rad, edge=edge)
     BSCatalogue = Stars_on_image(BSCatalogue, data, CRIT_m=max_mag)
     BSCatalogue = BSC_altaz(BSCatalogue, loc, time)
@@ -314,17 +392,21 @@ def processed(file, outfile, calibrate=True):  # Основная процеду
 
     BSCatalogue = BSC_theory_flux(BSCatalogue, wcs_file)
     BSCatalogue = BSC_observ_flux(BSCatalogue, data, STAR_PHOT=STAR_PHOT)
-
+    
     ascii.write(BSCatalogue, 'BSCatalogue.csv', format='csv', fast_writer=False)
     data = get_data(file)
-    make_map(BSCatalogue, file, data, outfile, lw=2.)
+    cloud = make_map(BSCatalogue, file, data[0], outfile, wcs_file, HIP_file, cnstl_file, lw=2.)
+    print("Output in: {}".format(outfile))
+    return camera_ip, t, cloud, ph, moon_alt, moon_az, sun_alt, sun_az, bkg_magnitd
 
 
-BSC_file = '../data/catalogs/BSC_clean.txt'  # Расположение каталога
+BSC_file = '../data/catalogs/BSCcat.csv'  # Расположение каталога
+HIP_file = '../data/catalogs/HIPcat.csv'
+cnstl_file = '../data/catalogs/constell.dat'
 
 lat = '43d44m46s'  # Широта места наблюдения (в таком формате)
 lon = '42d40m03s'  # Долгота места наблюдения (в таком формате)
-height = 2112  # Выоста места наблюдения (метры)
+height = 2112  # Высота места наблюдения (метры)
 TIME = '2019-09-24T17:18:23'  # Время UTC момента съёмки
 
 center_size = 200  # Область центра снимка "без" дисторсии для astrometry
@@ -333,7 +415,7 @@ scl_l = 10.  # Минимальный размер снимка (deg)
 scl_h = 100.  # Максимальный размер снимка (deg)
 tw_o = 0  # Порядок дисторсии (учитывается плохо)
 calibrate_m = 3.6  # Максимальная величина звёзд калибровки
-edge = 400  # насколько теоретически далеко за границу могут "вылезать" звёзды
+edge = 500  # насколько теоретически далеко за границу могут "вылезать" звёзды
 
 SNR = 5  # sigma для фильтра фона неба (pix)
 box_size = 50  # Размер box для фильтра фона неба (pix)
--- scripts/config.py
+++ scripts/config.py
@@ -2,16 +2,29 @@
 # -*- coding: UTF-8 -*-
 import check
 import glob
-import time
+import time as programm_time
+import psycopg2
+
+conn = psycopg2.connect(user='jorji', password='12345', host='localhost')
+cursor = conn.cursor()
 
 f = open('time.txt', 'w')
 files = glob.glob('../data/*.fits*')
 for i in range(len(files)):
-    start_time = time.time()
+    start_time = programm_time.time()
     outfile = '../processed/result_' + str(i) + '.jpg'  # Расположение output картинки
     if 'center' not in files[i]:
-        check.processed(files[i], outfile, calibrate=False)
-        f.write('file: ' + files[i] + '; time = ' + str(time.time() - start_time) + ' секунд' + '\n')
+        camera_ip, date_time, cloudiness, ph, moon_alt, moon_az, sun_alt, sun_az, bkg_mag = check.processed(files[i], outfile, calibrate=False)
+        
+        date = date_time.split('T')[0]
+        time = date_time.split('T')[1] #.split(':')
+        # time = time_array[0] + time_array[1] + time_array[2]
+        cursor.execute("INSERT INTO test_table2 (camera, date, time, cloudiness, moon_phase, altitude_moon, azimuth_moon, altitude_sun, azimuth_sun, background_magnitude, raw_file, cloud_map) \
+        	VALUES ({}, \'{}\', \'{}\', {}, {}, {}, {}, {}, {}, {}, \'{}\', \'{}\');".format(camera_ip, date, time, 1-cloudiness, ph, moon_alt, moon_az, sun_alt, sun_az, bkg_mag, files[i], outfile))
+        conn.commit()
+        f.write('file: ' + files[i] + '; time = ' + str(programm_time.time() - start_time) + ' секунд' + '\n')
 # outfile = '../processed/result_.jpg'
 # check.processed(files[2], outfile)
 f.close()
+cursor.close()
+conn.close()
